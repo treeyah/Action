cc9631886f4d7df45613f1216ec7afd7b41edbd1
treeyah <ben@kellerfamily.org>
2026-05-31 12:30:59 +0200

    Loops
    Added loops to the code

--- SpaceLang.cpp
+++ SpaceLang.cpp
@@ -7,70 +7,49 @@ using namespace std;
 int main() {
     string c;
     map<string, string> vars;
-    cout << "==SpaceLang==\n";
-    while (true) {
+    int repeatCount = 1;
 
+    cout << "==SpaceLang==\n";
+
+    while (true) {
         getline(cin, c);
 
-        // WRITE
-        if (c.find("write(") == 0) {
-            cout << c.substr(6, c.size() - 7);
-
+        if (c.find("repeat(") == 0) {
+            sscanf(c.c_str(), "repeat(%d)", &repeatCount);
         }
 
-        // ADD
+        else if (c.find("write(") == 0) {
+            for (int i = 0; i < repeatCount; i++) {
+                cout << c.substr(6, c.size() - 7) << "\n";
+            }
+            repeatCount = 1;
+        }
+
         else if (c.find("add(") == 0) {
             int a, b;
             sscanf(c.c_str(), "add(%d+%d)", &a, &b);
-            cout << a + b;
 
+            for (int i = 0; i < repeatCount; i++) {
+                cout << a + b << "\n";
+            }
+
+            repeatCount = 1;
         }
 
-        // MINUS
-        else if (c.find("minus(") == 0) {
-            int a, b;
-            sscanf(c.c_str(), "minus(%d-%d)", &a, &b);
-            cout << a - b;
-
-        }
-        // TIMES
-        else if (c.find("times(") == 0) {
-            int a, b;
-            sscanf(c.c_str(), "times(%d*%d)", &a, &b);
-            cout << a * b;
-
-        }
-        // DIVIDE
-        else if (c.find("divide(") == 0) {
-            int a, b;
-            sscanf(c.c_str(), "divide(%d/%d)", &a, &b);
-            cout << a / b;
-
-        }
-
-
-        // SET
-        else if (c.find("set(") == 0) {
-            string inside = c.substr(4, c.size() - 5);
-            int equals = inside.find('=');
-            string name = inside.substr(0, equals);
-            string value = inside.substr(equals + 1);
-            vars[name] = value;
-            cout << "Set!";
-
-        }
-
-        // GET
         else if (c.find("get(") == 0) {
             string name = c.substr(4, c.size() - 5);
-            cout << vars[name];
+
+            for (int i = 0; i < repeatCount; i++) {
+                cout << vars[name] << "\n";
+            }
+
+            repeatCount = 1;
         }
 
         else {
-            cout << "Code not found";
+            cout << "Code not found\n";
+            repeatCount = 1;
         }
-
-        cout << "\n";
     }
 
     return 0;
